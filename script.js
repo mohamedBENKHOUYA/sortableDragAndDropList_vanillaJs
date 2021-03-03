892f86f21dc5c0efccf83103ade972bcbca56ade
mohamed BEN KHOUYA <mohamed.benkhouya.1995@gmail.com>
2021-03-03 23:57:09 +0100

    last commit:

--- script.js
+++ script.js
@@ -18,11 +18,50 @@ draggables.forEach((draggable) => {
 
 containers.forEach((container) => {
     container.addEventListener('dragover', (e) => {
-        e.preventDefault();
         const draggable = document.querySelector('.dragging');
-        console.log(draggable);
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        console.log(afterElement);
+        if (afterElement === undefined) {
         container.appendChild(draggable);
+
+        } else {
+            container.insertBefore(draggable, afterElement);
+        }
+
+        // console.log(e.clientX);
+
+        // const elements = container.children;
+        // // console.log(elements);
+        // for (let element of elements) {
+        //     element.addEventListener('dragover', (e) => {
+        //         console.log(element);
+        //     })
+        // }
     })
 })
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        // console.log(box);
+        // if ( offset < 0 && offset > closest.offset ) {
+        //     return { offset: offset, element: child }
+        // } else {
+
+        // }
+         
+        return ( offset < 0 && offset > closest.offset )? 
+                {offset: offset, element: child}: closest;
+
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
+}
+
+
+
+
+
 
 
